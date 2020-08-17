4a2db0b17126bcb8ab21b574cdcdfda694caaaf9
Kaosilisochukwu Nwizu <kcharlse94@gmail.com>
2020-08-17 15:46:48 +0100

    build calculator functionality in the form project

--- UrbanCalculatorUI/UrbanCalculator.Designer.cs
+++ UrbanCalculatorUI/UrbanCalculator.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.visDisplay = new System.Windows.Forms.TextBox();
             this.deleteBtn = new System.Windows.Forms.Button();
             this.resetBtn = new System.Windows.Forms.Button();
             this.clearBtn = new System.Windows.Forms.Button();
@@ -51,226 +51,243 @@
             this.divisionBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox2
+            // visDisplay
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 12);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(334, 86);
-            this.textBox2.TabIndex = 0;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.visDisplay.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.visDisplay.Location = new System.Drawing.Point(12, 12);
+            this.visDisplay.Multiline = true;
+            this.visDisplay.Name = "visDisplay";
+            this.visDisplay.Size = new System.Drawing.Size(334, 54);
+            this.visDisplay.TabIndex = 0;
+            this.visDisplay.Text = "0";
+            this.visDisplay.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // deleteBtn
             // 
             this.deleteBtn.Font = new System.Drawing.Font("Segoe UI", 29F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.deleteBtn.Location = new System.Drawing.Point(12, 118);
+            this.deleteBtn.Location = new System.Drawing.Point(12, 82);
             this.deleteBtn.Name = "deleteBtn";
             this.deleteBtn.Size = new System.Drawing.Size(79, 84);
             this.deleteBtn.TabIndex = 1;
             this.deleteBtn.Text = "← ";
             this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
             // 
             // resetBtn
             // 
             this.resetBtn.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.resetBtn.Location = new System.Drawing.Point(97, 118);
+            this.resetBtn.Location = new System.Drawing.Point(97, 82);
             this.resetBtn.Name = "resetBtn";
             this.resetBtn.Size = new System.Drawing.Size(79, 84);
             this.resetBtn.TabIndex = 1;
             this.resetBtn.Text = "CE";
             this.resetBtn.UseVisualStyleBackColor = true;
+            this.resetBtn.Click += new System.EventHandler(this.resetBtn_Click);
             // 
             // clearBtn
             // 
             this.clearBtn.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.clearBtn.Location = new System.Drawing.Point(182, 118);
+            this.clearBtn.Location = new System.Drawing.Point(182, 82);
             this.clearBtn.Name = "clearBtn";
             this.clearBtn.Size = new System.Drawing.Size(79, 84);
             this.clearBtn.TabIndex = 1;
             this.clearBtn.Text = "C";
             this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
             // 
             // NegationBtn
             // 
             this.NegationBtn.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.NegationBtn.Location = new System.Drawing.Point(267, 118);
+            this.NegationBtn.Location = new System.Drawing.Point(267, 82);
             this.NegationBtn.Name = "NegationBtn";
             this.NegationBtn.Size = new System.Drawing.Size(79, 84);
             this.NegationBtn.TabIndex = 1;
             this.NegationBtn.Text = "±";
             this.NegationBtn.UseVisualStyleBackColor = true;
+            this.NegationBtn.Click += new System.EventHandler(this.NegationBtn_Click);
             // 
             // seven
             // 
             this.seven.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.seven.Location = new System.Drawing.Point(13, 208);
+            this.seven.Location = new System.Drawing.Point(13, 172);
             this.seven.Name = "seven";
             this.seven.Size = new System.Drawing.Size(79, 84);
             this.seven.TabIndex = 1;
             this.seven.Text = "7";
             this.seven.UseVisualStyleBackColor = true;
+            this.seven.Click += new System.EventHandler(this.numbers);
             // 
             // eight
             // 
             this.eight.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.eight.Location = new System.Drawing.Point(98, 208);
+            this.eight.Location = new System.Drawing.Point(98, 172);
             this.eight.Name = "eight";
             this.eight.Size = new System.Drawing.Size(79, 84);
             this.eight.TabIndex = 1;
             this.eight.Text = "8";
             this.eight.UseVisualStyleBackColor = true;
+            this.eight.Click += new System.EventHandler(this.numbers);
             // 
             // nine
             // 
             this.nine.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.nine.Location = new System.Drawing.Point(183, 208);
+            this.nine.Location = new System.Drawing.Point(183, 172);
             this.nine.Name = "nine";
             this.nine.Size = new System.Drawing.Size(79, 84);
             this.nine.TabIndex = 1;
             this.nine.Text = "9";
             this.nine.UseVisualStyleBackColor = true;
-            this.nine.Click += new System.EventHandler(this.button7_Click);
+            this.nine.Click += new System.EventHandler(this.numbers);
             // 
             // additionBtn
             // 
             this.additionBtn.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.additionBtn.Location = new System.Drawing.Point(268, 208);
+            this.additionBtn.Location = new System.Drawing.Point(268, 172);
             this.additionBtn.Name = "additionBtn";
             this.additionBtn.Size = new System.Drawing.Size(79, 84);
             this.additionBtn.TabIndex = 1;
             this.additionBtn.Text = "+";
             this.additionBtn.UseVisualStyleBackColor = true;
+            this.additionBtn.Click += new System.EventHandler(this.operators);
             // 
             // four
             // 
             this.four.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.four.Location = new System.Drawing.Point(13, 298);
+            this.four.Location = new System.Drawing.Point(13, 262);
             this.four.Name = "four";
             this.four.Size = new System.Drawing.Size(79, 84);
             this.four.TabIndex = 1;
             this.four.Text = "4";
             this.four.UseVisualStyleBackColor = true;
-            this.four.Click += new System.EventHandler(this.button9_Click);
+            this.four.Click += new System.EventHandler(this.numbers);
             // 
             // five
             // 
             this.five.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.five.Location = new System.Drawing.Point(98, 298);
+            this.five.Location = new System.Drawing.Point(98, 262);
             this.five.Name = "five";
             this.five.Size = new System.Drawing.Size(79, 84);
             this.five.TabIndex = 1;
             this.five.Text = "5";
             this.five.UseVisualStyleBackColor = true;
+            this.five.Click += new System.EventHandler(this.numbers);
             // 
             // six
             // 
             this.six.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.six.Location = new System.Drawing.Point(183, 298);
+            this.six.Location = new System.Drawing.Point(183, 262);
             this.six.Name = "six";
             this.six.Size = new System.Drawing.Size(79, 84);
             this.six.TabIndex = 1;
             this.six.Text = "6";
             this.six.UseVisualStyleBackColor = true;
+            this.six.Click += new System.EventHandler(this.numbers);
             // 
             // subtractionBtn
             // 
             this.subtractionBtn.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.subtractionBtn.Location = new System.Drawing.Point(268, 298);
+            this.subtractionBtn.Location = new System.Drawing.Point(268, 262);
             this.subtractionBtn.Name = "subtractionBtn";
             this.subtractionBtn.Size = new System.Drawing.Size(79, 84);
             this.subtractionBtn.TabIndex = 1;
             this.subtractionBtn.Text = "-";
             this.subtractionBtn.UseVisualStyleBackColor = true;
-            this.subtractionBtn.Click += new System.EventHandler(this.button12_Click);
+            this.subtractionBtn.Click += new System.EventHandler(this.operators);
             // 
             // one
             // 
             this.one.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.one.Location = new System.Drawing.Point(13, 388);
+            this.one.Location = new System.Drawing.Point(13, 352);
             this.one.Name = "one";
             this.one.Size = new System.Drawing.Size(79, 84);
             this.one.TabIndex = 1;
             this.one.Text = "1";
             this.one.UseVisualStyleBackColor = true;
+            this.one.Click += new System.EventHandler(this.numbers);
             // 
             // two
             // 
             this.two.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.two.Location = new System.Drawing.Point(98, 388);
+            this.two.Location = new System.Drawing.Point(98, 352);
             this.two.Name = "two";
             this.two.Size = new System.Drawing.Size(79, 84);
             this.two.TabIndex = 1;
             this.two.Text = "2";
             this.two.UseVisualStyleBackColor = true;
-            this.two.Click += new System.EventHandler(this.button14_Click);
+            this.two.Click += new System.EventHandler(this.numbers);
             // 
             // three
             // 
             this.three.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.three.Location = new System.Drawing.Point(183, 388);
+            this.three.Location = new System.Drawing.Point(183, 352);
             this.three.Name = "three";
             this.three.Size = new System.Drawing.Size(79, 84);
             this.three.TabIndex = 1;
             this.three.Text = "3";
             this.three.UseVisualStyleBackColor = true;
-            this.three.Click += new System.EventHandler(this.button15_Click);
+            this.three.Click += new System.EventHandler(this.numbers);
             // 
             // multiplicationBtn
             // 
             this.multiplicationBtn.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.multiplicationBtn.Location = new System.Drawing.Point(268, 388);
+            this.multiplicationBtn.Location = new System.Drawing.Point(268, 352);
             this.multiplicationBtn.Name = "multiplicationBtn";
             this.multiplicationBtn.Size = new System.Drawing.Size(79, 84);
             this.multiplicationBtn.TabIndex = 1;
             this.multiplicationBtn.Text = "x";
             this.multiplicationBtn.UseVisualStyleBackColor = true;
+            this.multiplicationBtn.Click += new System.EventHandler(this.operators);
             // 
             // period
             // 
             this.period.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.period.Location = new System.Drawing.Point(12, 478);
+            this.period.Location = new System.Drawing.Point(12, 442);
             this.period.Name = "period";
             this.period.Size = new System.Drawing.Size(79, 84);
             this.period.TabIndex = 1;
             this.period.Text = ".";
             this.period.UseVisualStyleBackColor = true;
+            this.period.Click += new System.EventHandler(this.numbers);
             // 
             // zero
             // 
             this.zero.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.zero.Location = new System.Drawing.Point(97, 478);
+            this.zero.Location = new System.Drawing.Point(97, 442);
             this.zero.Name = "zero";
             this.zero.Size = new System.Drawing.Size(79, 84);
             this.zero.TabIndex = 1;
             this.zero.Text = "0";
             this.zero.UseVisualStyleBackColor = true;
+            this.zero.Click += new System.EventHandler(this.numbers);
             // 
             // equaltoBtn
             // 
             this.equaltoBtn.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.equaltoBtn.Location = new System.Drawing.Point(182, 478);
+            this.equaltoBtn.Location = new System.Drawing.Point(182, 442);
             this.equaltoBtn.Name = "equaltoBtn";
             this.equaltoBtn.Size = new System.Drawing.Size(79, 84);
             this.equaltoBtn.TabIndex = 1;
             this.equaltoBtn.Text = "=";
             this.equaltoBtn.UseVisualStyleBackColor = true;
+            this.equaltoBtn.Click += new System.EventHandler(this.equaltoBtn_Click);
             // 
             // divisionBtn
             // 
             this.divisionBtn.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.divisionBtn.Location = new System.Drawing.Point(267, 478);
+            this.divisionBtn.Location = new System.Drawing.Point(267, 442);
             this.divisionBtn.Name = "divisionBtn";
             this.divisionBtn.Size = new System.Drawing.Size(79, 84);
             this.divisionBtn.TabIndex = 1;
             this.divisionBtn.Text = "÷";
             this.divisionBtn.UseVisualStyleBackColor = true;
-            this.divisionBtn.Click += new System.EventHandler(this.button20_Click);
+            this.divisionBtn.Click += new System.EventHandler(this.operators);
             // 
             // UrbanCalculator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(359, 579);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(359, 553);
             this.Controls.Add(this.divisionBtn);
             this.Controls.Add(this.multiplicationBtn);
             this.Controls.Add(this.equaltoBtn);
@@ -291,9 +308,13 @@
             this.Controls.Add(this.seven);
             this.Controls.Add(this.resetBtn);
             this.Controls.Add(this.deleteBtn);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.visDisplay);
+            this.MaximumSize = new System.Drawing.Size(377, 610);
+            this.MinimumSize = new System.Drawing.Size(377, 600);
             this.Name = "UrbanCalculator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "UrbanCalculator";
+           // this.Load += new System.EventHandler(this.UrbanCalculator_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -301,7 +322,7 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox visDisplay;
         private System.Windows.Forms.Button deleteBtn;
         private System.Windows.Forms.Button resetBtn;
         private System.Windows.Forms.Button clearBtn;
--- UrbanCalculatorUI/UrbanCalculator.cs
+++ UrbanCalculatorUI/UrbanCalculator.cs
@@ -10,49 +10,112 @@ namespace UrbanCalculatorUI
 {
     public partial class UrbanCalculator : Form
     {
+        string mathOperator = "";
+        double currentAns, operated;
+        bool reset = false;
+
+
         public UrbanCalculator()
         {
             InitializeComponent();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void numbers(object sender, EventArgs e)
         {
+            Button addDigit = (Button)sender;
+            if(visDisplay.Text.Length < 16)
+            {
+                if (visDisplay.Text == "0")
+                    visDisplay.Text = "";
+                if (addDigit.Text == "." && reset == true)
+                {
+                    if (visDisplay.Text == "")
+                        visDisplay.Text += "0";
+                    if (!visDisplay.Text.Contains("."))
+                        visDisplay.Text += addDigit.Text;
+                }
+                else if (mathOperator != "" && reset == true)
+                {
+                    visDisplay.Text = "";
+                    visDisplay.Text += addDigit.Text;
+                    reset = false;
+                }
+                else
+                    visDisplay.Text += addDigit.Text;
+                operated = double.Parse(visDisplay.Text);
+            }
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            if (visDisplay.Text.Length > 1)
+                visDisplay.Text = visDisplay.Text.Remove(visDisplay.Text.Length - 1);
+            else
+                visDisplay.Text = "0";
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            visDisplay.Text = "0";
+            operated = 0;
+        }
+
+        private void resetBtn_Click(object sender, EventArgs e)
+        {
+            visDisplay.Text = "0";
+            mathOperator = "";
+            currentAns = 0;
+            operated = 0;
+        }
+
+        //private void UrbanCalculator_Load(object sender, EventArgs e)
+        //{
+
+        //}
+
+
+        private void operators(object sender, EventArgs e)
+        {
+            Button currentOperator = (Button)sender;
+            if (mathOperator == "+")
+                visDisplay.Text = (currentAns + operated).ToString();
+            else if (mathOperator == "-")
+                visDisplay.Text = (currentAns - operated).ToString();
+            else if (mathOperator == "x")
+                visDisplay.Text = (currentAns * operated).ToString();
+            else if (mathOperator == "÷")
+                visDisplay.Text = (currentAns / operated).ToString();
+            currentAns = double.Parse(visDisplay.Text);
+            mathOperator = currentOperator.Text;
+            operated = mathOperator == "+" || mathOperator == "-" ? 0 : 1;
+            reset = true; 
 
         }
 
-        private void button7_Click(object sender, EventArgs e)
+        private void NegationBtn_Click(object sender, EventArgs e)
         {
-
+            if (visDisplay.Text.Contains("-"))
+                visDisplay.Text = visDisplay.Text.Remove(0, 1);
+            else
+                visDisplay.Text = "-" + visDisplay.Text;
+            operated = double.Parse(visDisplay.Text);
         }
 
-        private void button20_Click(object sender, EventArgs e)
+        private void equaltoBtn_Click(object sender, EventArgs e)
         {
-
+            if (mathOperator == "+")
+                visDisplay.Text = (currentAns + operated).ToString();
+            else if(mathOperator == "-")
+                visDisplay.Text = (currentAns - operated).ToString();
+            else if(mathOperator == "x")
+                visDisplay.Text = (currentAns * operated).ToString();
+            else if(mathOperator == "÷")
+                visDisplay.Text = (currentAns / operated).ToString();
+            reset = true;
+            currentAns = double.Parse(visDisplay.Text);
+            mathOperator = "";
+            operated = 0;
         }
 
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button12_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button14_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button15_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button9_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
